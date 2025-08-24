8e8d0fee61a78d745f3df3bcb8f35b6652398233
Jordan-Stephens <JordanKeithStephens@gmail.com>
2025-08-23 20:21:21 -0400

    Added HTMLNode class

--- src/main.py
+++ src/main.py
@@ -1,8 +1,14 @@
 from textnode import TextNode, TextType
+from htmlnode import HTMLNode
 
+proptest = {
+    "href": "https://www.google.com",
+    "target": "_blank",
+}
 
 def main():
-    test_node = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
+    test_node = HTMLNode("<a>", "some text", props=proptest)
     print(test_node)
+    print(test_node.props_to_html())
 
 main()
