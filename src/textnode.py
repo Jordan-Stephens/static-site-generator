a194c5464ed70e0c3d64479fba96d741cfc54504
Jordan-Stephens <JordanKeithStephens@gmail.com>
2025-08-24 19:38:41 -0400

    bug fix in textnodetohtmlnode in textnode.py

--- src/textnode.py
+++ src/textnode.py
@@ -31,13 +31,13 @@ class TextNode:
 
 def text_node_to_html_node(text_node):
     if text_node.text_type == TextType.TEXT:
-        return LeafNode(None, LeafNode.text)
+        return LeafNode(None, text_node.text)
     elif text_node.text_type == TextType.BOLD:
-        return LeafNode("b", LeafNode.text)
+        return LeafNode("b", text_node.text)
     elif text_node.text_type == TextType.ITALIC:
-        return LeafNode("i", LeafNode.text)
+        return LeafNode("i", text_node.text)
     elif text_node.text_type == TextType.CODE:
-        return LeafNode("code", LeafNode.text)
+        return LeafNode("code", text_node.text)
     elif text_node.text_type == TextType.LINK:
         return LeafNode("a", text_node.text, {"href": text_node.url})
     elif text_node.text_type == TextType.IMAGE:
